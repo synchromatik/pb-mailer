94d37f3d0846b0b34670b114bc66eab23c21695f
Marko Manojlovic <marko@ivyexec.com>
2019-08-05 16:20:40 +0200

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const nodemailer = require('nodemailer');
+
+require('dotenv').config()
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
