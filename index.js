592c088a1a9ddf79bc0473a679af424548337301
Marko Manojlovic <marko@ivyexec.com>
2019-08-05 17:00:41 +0200

    body parser added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const nodemailer = require('nodemailer');
-
 require('dotenv').config()
-const app = express()
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
 
+const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => 
+    res.send('Hello World!'))
+
+app.post('/email', (req, res) => {
+    const { email = '', name = '', message = '' } = req.body
+    res.send(`Post Test ${email} ${name} ${message}`)
+})
+    
+
+app.listen(port, () => 
+    console.log(`Example app listening on port ${process.env.PORT}!`))
