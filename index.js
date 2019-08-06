b04d5c6e46ecccb31379e681e5bd6e4622121013
Marko Manojlovic <marko@ivyexec.com>
2019-08-06 17:24:12 +0200

    working post route for mailgun

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-const express = require('express')
 require('dotenv').config()
-const nodemailer = require('nodemailer')
+const express = require('express')
+var cors = require('cors')
+const mailgun = require("mailgun-js")
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .use(cors())
+    .options('*', cors())
 
 app.get('/', (req, res) => 
     res.send('Hello World!'))
 
 app.post('/email', (req, res) => {
-    const { email = '', name = '', message = '' } = req.body
-    res.send(`Post Test ${email} ${name} ${message}`)
+    // Request from react
+    const { email, name, message } = req.body
+    // Mailgun
+    const mg = mailgun({apiKey: process.env.MAILGUN_API, domain: process.env.MAILGUN_DOMAIN});
+    // Pack it
+    const data = {
+        from: email,
+        to: process.env.TO_EMAIL,
+        subject: `Poruka sa sajta pickled-brain.com od ${name}`,
+        text: message
+    };
+    // Send it 
+    mg.messages().send(data, function (error, body) {
+        if (error) {
+            res.send(body)
+        } else {
+            res.send(`Poruka poslata`)
+        }
+    });
 })
     
-
 app.listen(port, () => 
     console.log(`Example app listening on port ${process.env.PORT}!`))
